b795db18b98ce340c1c874df6593000bd0341552
江小鉴 <abyss_er@163.com>
2024-09-05 16:10:36 +0800

    Signed-off-by: 江小鉴 <abyss_er@163.com>

--- main.c
+++ main.c
@@ -29,8 +29,8 @@ struct jdos_task{
 	int *stack_sp;												//当前任务的堆栈指针
 	struct jdos_task *next;								//指向链表下一个节点
 };
-struct jdos_task *jdos_task_list_frist= NULL;	//创建系统链表指针，用于保存链表第一个任务位置
-struct jdos_task *jdos_task_list = NULL;	//创建一个任务链表指针
+struct jdos_task *jdos_task_sp_frist= NULL;	//创建系统链表指针，用于保存链表第一个任务位置
+struct jdos_task *jdos_task_sp = NULL;	//创建一个任务链表指针
 
 /*申请任务空间
 *	jdos_task_sp：链表指针
@@ -57,17 +57,17 @@ struct jdos_task *jdos_create_task(void (*task_entry),unsigned int stack_size)
 {
 	struct jdos_task *jdos_new_task = NULL;	//创建一个任务链表指针
 	if(jdos_request_space(jdos_new_task,stack_size)==JDOS_ERR)return NULL;	//申请空间
-	jdos_new_task->previous = jdos_task_list;																//当前节点的previous指向上一个节点
-	jdos_new_task->next = jdos_task_list_frist;															//当前节点的next指向第一个节点
-	jdos_task_list->next = jdos_new_task;																		//上一节点的next指向当前节点
-	jdos_task_list_frist->previous = jdos_new_task;													//第一个节点的previous指向当前节点
-	jdos_task_list = jdos_new_task;																					//链表指针移动到当前节点
+	jdos_new_task->previous = jdos_task_sp;																//当前节点的previous指向上一个节点
+	jdos_new_task->next = jdos_task_sp_frist;															//当前节点的next指向第一个节点
+	jdos_task_sp->next = jdos_new_task;																		//上一节点的next指向当前节点
+	jdos_task_sp_frist->previous = jdos_new_task;													//第一个节点的previous指向当前节点
+	jdos_task_sp = jdos_new_task;																					//链表指针移动到当前节点
 	
-	jdos_task_list->task_entry = task_entry;																//任务入口
-	jdos_task_list->jdos_task_statu = JDOS_TASK_PAUSE;                      //创建任务，状态为暂停状态，等待启动
-	jdos_task_list->stack_size = stack_size;																//记录当前任务堆栈大小
+	jdos_task_sp->task_entry = task_entry;																//任务入口
+	jdos_task_sp->jdos_task_statu = JDOS_TASK_PAUSE;                      //创建任务，状态为暂停状态，等待启动
+	jdos_task_sp->stack_size = stack_size;																//记录当前任务堆栈大小
 	
-	return jdos_task_list;																									//返回当前任务节点
+	return jdos_task_sp;																									//返回当前任务节点
 }
 
 /*任务开执行
@@ -89,7 +89,7 @@ int jdos_delete_task(struct jdos_task *jdos_task)
 	struct jdos_task *jdos_task_previous = jdos_task->previous;
 	struct jdos_task *jdos_task_next = jdos_task->next;
 	
-	if(jdos_task==jdos_task_list_frist)return JDOS_ERR;	//判断是否为系统节点，系统节点不可删除
+	if(jdos_task==jdos_task_sp_frist)return JDOS_ERR;	//判断是否为系统第一个节点，系统第一个节点不可删除
 	
 	jdos_task_previous->next = jdos_task_next;					//上一个节点的next指向下一个节点
 	jdos_task_next->previous = jdos_task_previous;			//下一个节点的previous指向上一个节点
@@ -102,15 +102,15 @@ int main(void);
 /*jdos初始化*/
 int jdos_init()
 {					
-	if(jdos_request_space(jdos_task_list_frist,JDOS_MAIN_STACK_SIZE)==JDOS_ERR)return JDOS_ERR;	//申请空间
-	jdos_task_list_frist->previous = jdos_task_list;																						//节点与自身相连
-	jdos_task_list_frist->next = jdos_task_list;																 								//节点与自身相连
+	if(jdos_request_space(jdos_task_sp_frist,JDOS_MAIN_STACK_SIZE)==JDOS_ERR)return JDOS_ERR;	//申请空间
+	jdos_task_sp_frist->previous = jdos_task_sp;																						//节点与自身相连
+	jdos_task_sp_frist->next = jdos_task_sp;																 								//节点与自身相连
 	
-	jdos_task_list->task_entry = main;																													//任务入口
+	jdos_task_sp->task_entry = main;																													//任务入口
 
-	jdos_task_list_frist->jdos_task_statu = JDOS_TASK_READY;																		//就绪状态，直接运行								
-	jdos_task_list_frist->stack_size = JDOS_MAIN_STACK_SIZE;																		//堆栈大小
-	jdos_task_list = jdos_task_list_frist;																											//节点移入链表
+	jdos_task_sp_frist->jdos_task_statu = JDOS_TASK_READY;																		//就绪状态，直接运行								
+	jdos_task_sp_frist->stack_size = JDOS_MAIN_STACK_SIZE;																		//堆栈大小
+	jdos_task_sp = jdos_task_sp_frist;																											//更新节点指针
 	main();
 	return JDOS_OK;
 }
