fa21175e612cf0b769223009784051df917ba310
abyss_er@163.com <abyss_er@163.com>
2024-09-14 11:10:56 +0800

    修改systickk

--- jdos.c
+++ jdos.c
@@ -441,7 +441,9 @@ void HAL_IncTick(void)
 
 	while (jd_task->timeout == jd_time)
 	{
-		jd_node_insert(jd_task->node->previous, JD_NULL, jd_task->node->next); // 删除当前节点
+		jd_node_insert(jd_task->node->previous, JD_NULL, jd_task->node->next); // 删除延时完成的节点
+		jd_task_list_reading = jd_node_delete(jd_task_list_reading,jd_task_list_reading);
+		
 		jd_task_run(jd_task);												   // 加入就绪链表
 		jd_task_list_delaying = jd_task_list_delaying->next;				   // 切换表头
 		jd_task = jd_task_list_delaying->addr;								   // 获取任务数据
