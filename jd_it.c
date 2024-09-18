d3fa6c9a190c5083fb3ea932bc55c7e2cd1827b3
江小鉴 <abyss_er@163.com>
2024-09-18 16:21:04 +0800

    分散函数

--- jd_it.c
+++ jd_it.c
@@ -1,24 +1,30 @@
 #include "jdos.h"
 
-
-/*jdos延时，让出CPU使用权
- * ms:延时时间，单位ms
- */
-void jd_delay(jd_uint32_t ms)
+/*hal库已自动使能systick，以下为hal库systick异常回调函数*/
+void HAL_IncTick(void)
 {
-	if (ms == 0)
-		return;
-	jd_task_runing->status = JD_DELAY;
-	jd_task_runing->timeout = jd_time + ms; // 将延时时间写入节点
-	// 删除就绪链表中的节点
-	jd_task_list_readying = jd_node_delete(jd_task_list_readying, jd_task_runing->node);
+	uwTick += uwTickFreq; // 系统自带不可删除,否则hal_delay等hal库函数不可用
 
-	// 将节点加入延时链表
-	jd_task_list_delaying = jd_node_in_rd(jd_task_list_delaying, jd_task_runing->node);
+	jd_time++; // jd_lck++
+	// 判断延时表头是否到达时间，若没有到达时间，则切换，若到达时间则将任务加入到就绪任务,在切换任务
+	jd_task_t *jd_task;
+	jd_task = jd_task_list_delaying->addr; // 获取任务数据
+	while (jd_task->timeout == jd_time)
+	{
+		jd_task = jd_task_list_delaying->addr;												  // 获取任务数据
+		jd_task_list_delaying = jd_node_delete(jd_task_list_delaying, jd_task_list_delaying); // 删除延时完成的节点
+		jd_task_run(jd_task);																  // 加入就绪链表
+		
+		if(jd_task_list_delaying == JD_NULL)
+			break;
+	}
+	jd_asm_pendsv_putup();
+}
 
-	// 切换线程，让出CPU，等延时后调度，用svc指令
-	
-	
+
+/*PendSV处理函数*/
+ void jd_PendSV_Handler(void)
+ {
 	jd_task_t *jd_task;
 	jd_node_list_t *node_temp;
 	node_temp = jd_task_list_readying;
@@ -52,22 +58,9 @@ void jd_delay(jd_uint32_t ms)
 	jd_task_stack_sp = &jd_task_runing->stack_sp;	   // 更新当前任务全局栈指针变量
 	jd_task_runing = jd_task;						   // 更改当前为运行的任务
 	jd_task_next_stack_sp = &jd_task_runing->stack_sp; // 更新下一个任务全局栈指针变量
-	
-	
-	jd_asm_svc_call();
+
+	jd_asm_pendsv_handler(); //切换上下文
 }
+ 
 
 
-
-/*timer创建*/
-jd_task_t *jd_timer_create(jd_task_t *task)
-{
-    
-}
-
-
-/*timer删除*/
-jd_int32_t jd_timer_delete(jd_task_t *task)
-{
-
-}
---
+++ jd_main.c
@@ -0,0 +1,57 @@
+#include "jdos.h"
+
+jd_task_t *test_task1, *test_task2, *test_task3;
+// 测试任务
+void task1()
+{
+	// printf("1 hello\r\n");
+	// while (1)
+	{
+	jd_delay(100);
+	 HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_7);
+		};
+}
+void task2()
+{
+	// printf("2 hello\r\n");
+	while (1)
+	{
+		jd_delay(500);
+		//jd_task_run(test_task1);
+		// HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_7);
+	};
+}
+void task3()
+{
+	// printf("3 hello\r\n");
+	while (1)
+	{
+		jd_delay(500);
+		//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+	};
+}
+
+/*系统main,系统第一个任务，不可使用jd_task_delete删除，可添加其他任务初始化代码*/
+__weak void jd_main(void)
+{
+	// printf("jd hello\r\n");
+	test_task1 = jd_task_create(task1, 512, 3);
+	if (test_task1 != JD_NULL)
+		jd_task_run(test_task1);
+
+	test_task2 = jd_task_create(task2, 512, 1);
+	if (test_task1 != JD_NULL)
+		jd_task_run(test_task2);
+
+	test_task3 = jd_task_create(task3, 512, 2);
+	if (test_task1 != JD_NULL)
+		jd_task_run(test_task3);
+	while (1)
+	{
+		//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+
+		// 注意此处调用延时切换任务，如果所有任务都不为就绪状态，程序将死循环，直到有就绪任务才会切换
+		// 应该在此处休眠或者其他不重要的工作
+		HAL_Delay(100);
+	};
+}
