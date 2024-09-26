bfa7b823490b71a0a214f183d2fa6b57844f34d3
江小鉴 <abyss_er@163.com>
2024-09-26 17:23:12 +0800

    初始化串口1

---
+++ jd_printf.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: 江小鉴 abyss_er@163.com
+ * @Date: 2024-09-26 17:00:09
+ * @LastEditors: 江小鉴 abyss_er@163.com
+ * @LastEditTime: 2024-09-26 17:21:22
+ * @FilePath: \jd_rtos\jd_printf.c
+ * @Description: jd_printf打印
+ */
+#include "jdos.h"
+
+void jd_printf(void)
+{
+}
